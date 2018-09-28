82df422445e05c3fd2cef30a6e408a77174c24c6
phani.thontepu <phani.thontepu@infosys.com>
2018-09-28 14:19:00 +0530

    files added to source

--- src/Sample.py
+++ src/Sample.py
@@ -1,51 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import matplotlib
 import numpy as np
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-from matplotlib import style
-import warnings
-from math import sqrt
-from collections import Counter
-style.use('fivethirtyeight')
 
-def k_nearest_neighbors(data, predict, k=3):
-    print( len(data),'Length of data+')
-    if len(data) >= k:
-        warnings.warn('K is set to a value less than total voting groups!')
-        
-    distances = []
-    for group in data:
-        print(group ,'Group being searched')
-        for features in data[group]:
-            print(features,'Features being compared')
-            euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
-            #print(euclidean_distance,' this distance for feature',features)
-            distances.append([euclidean_distance,group])
-    
-    print(distances)
-    print('---------------------------')
-    for i in sorted(distances)[:k]:
-        print(i[1])
-    print('---------------------------')
 
-    votes = [i[1] for i in sorted(distances)[:k]]
-    print(votes)
-    print(Counter(votes))
-    print(Counter(votes).most_common(1))
-    vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+delta = 1
+x = np.arange(-3.0, 3.0, delta)
+y = np.arange(-2.0, 2.0, delta)
+X, Y = np.meshgrid(x, y)
+Z1 = np.exp(-X**2 - Y**2)
+Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
+Z = (Z1 - Z2) * 2
 
-dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
-new_features = [5,7]
-[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
-# same as:
-##for i in dataset:
-##    for ii in dataset[i]:
-##        plt.scatter(ii[0],ii[1],s=100,color=i)
-        
-plt.scatter(new_features[0], new_features[1], s=100)
-
-result = k_nearest_neighbors(dataset, new_features)
-print(result)
-plt.scatter(new_features[0], new_features[1], s=100, color = result)  
-plt.show()
+fig, ax = plt.subplots()
+CS = ax.contour(X, Y, Z)
+ax.clabel(CS, inline=1, fontsize=10)
+ax.set_title('Simplest default with labels')
--- src/k_nearest_neighbors_using_scikit_learn.py
+++ src/k_nearest_neighbors_using_scikit_learn.py
@@ -1,29 +1,69 @@
 # -*- coding: utf-8 -*-
 
-#Imports 
+# Imports 
+#import os
+import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from os import path
 
-# Iris Dataset
+'''
+# Setting paths
+cwd = os.getcwd()
+'''
 # Importing the dataset
-dataset = pd.read_csv('iris.csv')
+
+'''
+# Iris Dataset
+dataset = pd.read_csv(path.join('..','data','iris.csv'))
+X_columns = ['sepal length', 'sepal width', 'petal length', 'petal width']
+y_columns = ['iris']
+
+'''
+
+# Social Network Ads Dataset
+dataset = pd.read_csv(path.join('..','data','Social_Network_Ads.csv'))
+X_columns = [ 'Age', 'EstimatedSalary']
+y_columns = ['Purchased']
 
 # Extracting Features and Labels
-dataset_feature = dataset.drop(labels = ['iris'],axis = 1)
-dataset_labels = dataset['iris']
+dataset_feature = dataset[X_columns]
+dataset_labels = dataset[y_columns]
+
+
 
 # Test Train Split
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(dataset_feature,dataset_labels,test_size=0.25)
 
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
 # Importing the classifier and Training it
 from sklearn.neighbors import KNeighborsClassifier
-knnclf = KNeighborsClassifier()
-knnclf.fit(X =X_train.values ,y =y_train.values)
+knnclf = KNeighborsClassifier(n_neighbors=5,metric = 'minkowski', p = 2)
+knnclf.fit( X =X_train ,y = y_train.values.ravel())
 
 # Predicting based on trained classifier
-y_pred = knnclf.predict(X=X_test.values)
+y_pred = knnclf.predict(X=X_test)
 
 # Obtaining the confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_true= y_test ,y_pred= y_pred)
+
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['r','g'])
+x_axis = dataset[X_columns[0]]
+y_axis = dataset[X_columns[1]]
+plt.scatter(x_axis.values,y_axis.values)
+plt.show()
+x_axis_grid = np.arange(x_axis.min()-1,x_axis.max()+1,step = 0.1)
+y_axis_grid = np.arange(y_axis.min()-1,y_axis.max()+1,step = 0.1)
+XX,yy = np.meshgrid(x_axis_grid,y_axis_grid)
+z = knnclf.predict(X=np.array([XX.ravel(),yy.ravel()]).T).reshape(XX.shape)
+plt.contourf(XX,yy,z,cmap = cmap)
+plt.show()
+
